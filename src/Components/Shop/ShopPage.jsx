da5f9368d138262a5a361e233f6ee3eb69aa2b41
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-24 22:00:59 +0600

    Refactor FetchProduct function to use async/await for improved readability and error handling

--- src/Components/Shop/ShopPage.jsx
+++ src/Components/Shop/ShopPage.jsx
@@ -10,20 +10,37 @@ const ShopPage = () => {
   const [CurrentPage, setCurrentPage] = useState(1);
 
   useEffect( () => {
-    
     FetchProduct()
-  }, [CurrentPage] )
+  }, [CurrentPage]);
 
 
-  const FetchProduct = () => {
-    setIsLoading(true)
-    apiClient.get(`/products/?page=${CurrentPage}`)
-    .then( res => {
-      setProduct(res.data.results)
-      setTotalPages( Math.ceil( res.data.count / res.data.results.length ) )
-    })
-    .catch( error => console.log(error) )
-    .finally( () => setIsLoading(false) )
+  // const FetchProduct = () => {
+  //   setIsLoading(true)
+  //   apiClient.get(`/products/?page=${CurrentPage}`)
+  //   .then( res => {
+  //     setProduct(res.data.results)
+  //     setTotalPages( Math.ceil( res.data.count / res.data.results.length ) )
+  //   })
+  //   .catch( error => console.log(error) )
+  //   .finally( () => setIsLoading(false) )
+  // }
+
+  const FetchProduct = async() => {
+    setIsLoading(true);
+    
+    try {
+
+      const response = await apiClient.get(`/products/?page=${CurrentPage}`);
+      const data = await response.data;
+
+      setProduct(data.results);
+      setTotalPages( Math.ceil( data.count / data.results.length ) )
+
+    } catch ( error ) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
