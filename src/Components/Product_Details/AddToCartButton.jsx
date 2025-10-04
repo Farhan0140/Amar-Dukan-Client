00bf95ecec11fc599a13174d8bbf57f79e63258e
Farhan Nadim <farhannadim2023@gmail.com>
2025-10-04 21:38:58 +0600

    36.2 Add To Cart Button Stock Update

---
+++ src/Components/Product_Details/AddToCartButton.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { FaMinus, FaPlus, FaShoppingCart } from "react-icons/fa";
+
+const AddToCartButton = ({ product }) => {
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    if ( quantity < product.stock ) {
+      setQuantity( quantity + 1 );
+    }
+  }
+
+  const decreaseQuantity = () => {
+    if( quantity > 1 ) {
+      setQuantity( quantity - 1 );
+    }
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="join">
+        <button 
+          className="btn btn-outline join-item"
+          onClick={decreaseQuantity}
+          disabled={ quantity <= 1 }
+        >
+          <FaMinus className="h-3 w-4" />
+        </button>
+        <input 
+          type="number"
+          min={1}
+          max={product.stock}
+          value={quantity}
+          className="input input-bordered join-item w-16 text-center [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::webkit-outer-spin-button]:appearance-none"
+        />
+        <button 
+          className="btn btn-outline join-item"
+          onClick={increaseQuantity}
+          disabled={quantity >= product.stock}
+        >
+          <FaPlus className="h-3 w-4" />
+        </button>
+      </div>
+
+      <button className="btn btn-primary w-full">
+        <span className="flex items-center">
+          <FaShoppingCart className="mr-2 h-4 w-4" />
+          Add To Cart
+        </span>
+      </button>
+
+    </div>
+  );
+};
+
+export default AddToCartButton;
